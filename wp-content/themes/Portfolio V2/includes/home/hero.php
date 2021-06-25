e4908976f0b9fddbc2709b6d4de845cedbc6009a
Helvin Rymer <Helvin159@gmail.com>
2021-06-25 19:54:11 -0400

    updates

--- wp-content/themes/Portfolio V2/includes/home/hero.php
+++ wp-content/themes/Portfolio V2/includes/home/hero.php
@@ -2,7 +2,7 @@
     <div class="container text-center py-5 mt-5">
         <h1 class="d-4 mt-5 permanent-marker">Front-End &amp; Wordpress Developer</h1>
         <p class="mb-0">War is not the answer, for only love can conquer hate!</p>
-        <?php print_r($_SERVER['DOCUMENT_ROOT']);?>
+        <?php // print_r($_SERVER['DOCUMENT_ROOT']);?>
     </div>
     <div class="container py-3 text-center">
         <img src="<?php echo get_theme_file_uri('assets/Octocat1.png')?>" style="max-width: 30rem;border-radius: 50%;" alt="">
