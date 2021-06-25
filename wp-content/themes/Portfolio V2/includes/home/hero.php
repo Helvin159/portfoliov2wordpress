8df1e61c90d2280b703db8604c5a74a53f1314a2
Helvin Rymer <Helvin159@gmail.com>
2021-06-25 19:54:58 -0400

    updates

--- wp-content/themes/Portfolio V2/includes/home/hero.php
+++ wp-content/themes/Portfolio V2/includes/home/hero.php
@@ -12,10 +12,8 @@
             <?php  
             if($_SERVER['SERVER_NAME'] !== 'myportfolio.local/'){
                 require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/portfolio v2/includes/svg/city.php');
-            }else {
-                // require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/portfolio v2/includes/svg/city.php');
-                print_r($_SERVER['DOCUMENT_ROOT']);
-            }?>
+            }
+            ?>
             
         </div>
     </div>
