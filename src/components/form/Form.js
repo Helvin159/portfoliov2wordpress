4b653c213a10e05b4a545102b99281f94784e5f0
Helvin Rymer <Helvin159@gmail.com>
2021-07-12 14:52:44 -0400

    updates

---
+++ src/components/form/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+	return (
+		<form action='#!'>
+			<input type='text' name='firstName' />
+			<input type='text' name='lastName' />
+			<input type='email' name='email' />
+		</form>
+	);
+};
+
+export default Form;
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div>
+			<h1>Portfolio V3</h1>
+		</div>
+	);
+};
+
+export default Hero;
